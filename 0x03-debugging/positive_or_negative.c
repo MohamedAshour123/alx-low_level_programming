646ae729108a5b131961718b84c9ca68081011c4
Mohamed Ashour <ashour4301@gmail.com>
2023-07-20 19:18:03 -0700

    l

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 
-void positive_or_negative(int i) {
-  if (i > 0) {
-    printf("%d is positive\n", i);
-  } else if (i < 0) {
-    printf("%d is negative\n", i);
-  } else {
-    printf("%d is zero\n", i);
-  }
+void positive_or_negative(int i)
+{
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i < 0)
+}
+printf("%d is negative\n", i);
+}
+else
+{
+printf("%d is zero\n", i);
+}
 }
 
-int main() {
-  int number;
-  printf("Enter a number: ");
-  scanf("%d", &number);
-  positive_or_negative(number);
-  return 0;
+int main(void)
+{
+int number;
+printf("Enter a number: ");
+scanf("%d", &number);
+positive_or_negative(number);
+return (0);
 }

d60ae135ccd915ba9e3d0e6704300e8dbf3ac5d3
Mohamed Ashour <ashour4301@gmail.com>
2023-08-11 20:05:17 -0700

    b

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,7 +17,7 @@ int i = 0;
 while (s[i])
 {
 if (s[i] < '0' || s[i] > '9')
-	return (0);
+return (0);
 i++;
 }
 return (1);
@@ -26,13 +26,11 @@ return (1);
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
 int i = 0;
-
 while (s[i] != '\0')
 {
 i++;
@@ -41,7 +39,7 @@ return (i);
 }
 
 /**
- * errors - handles errors for main
+ * error - handles errors for main
  */
 void errors(void)
 {
@@ -50,7 +48,7 @@ exit(98);
 }
 
 /**
- * main - multiplies two positive numbers
+ *main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -62,7 +60,7 @@ char *s1, *s2;
 int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 s1 = argv[1], s2 = argv[2];
-if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+if (argc != 3 || !is_digits(s1) || !is_digit(s2))
 errors();
 len1 = _strlen(s1);
 len2 = _strlen(s2);
@@ -76,25 +74,26 @@ for (len1 = len1 - 1; len1 >= 0; len1--)
 {
 digit1 = s1[len1] - '0';
 carry = 0;
+for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
-	  digit2 = s2[len2] - '0';
-	  carry += result[len1 + len2 + 1] + (digit1 *digit2);
-	  result[len1 + len2 + 1] = carry % 10;
-	  carry /= 10;
-	}
+digit2  = s2[len2] - '0';
+carry += result[len1 + len2 + 1] + (digit1 *digit2);
+result[len1 + len2 + 1] = carry % 10;
+carry /= 10;
+}
 if (carry > 0)
-	result[len1 + len2 + 1] += carry;
+result[len1 + len2 + 1] += carry;
 }
 for (i = 0; i < len - 1; i++)
 {
 if (result[i])
-	a = 1;
+a = 1;
 if (a)
-	_putchar(result[i] + '0');
+_putchar(result[i] + '0');
 }
 if (!a)
 _putchar('0');
-_putchar('\n');
+_putchar("\n");
 free(result);
 return (0);
 }
